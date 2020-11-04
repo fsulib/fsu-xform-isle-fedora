c33ad3e897966591407f63672b444bb54170be9f
Malcolm Shackelford <mshackelford@fsu.edu>
2020-11-04 15:29:24 -0500

    Actually included the shell script.

---
+++ set_db_endpoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo $CODEBUILD_WEBHOOK_HEAD_REF | grep -q 'local' && export DB_ENDPOINT=$DB_ENDPOINT_LOCAL
+echo $CODEBUILD_WEBHOOK_HEAD_REF | grep -q 'test' && export DB_ENDPOINT=$DB_ENDPOINT_TEST
+echo $CODEBUILD_WEBHOOK_HEAD_REF | grep -q 'prod' && export DB_ENDPOINT=$DB_ENDPOINT_PROD
+echo $CODEBUILD_WEBHOOK_HEAD_REF | grep -q 'local' && export IMAGE_TAG=local
+echo $CODEBUILD_WEBHOOK_HEAD_REF | grep -q 'test' && export IMAGE_TAG=test
+echo $CODEBUILD_WEBHOOK_HEAD_REF | grep -q 'prod' && export IMAGE_TAG=prod
+test -z $CODEBUILD_WEBHOOK_HEAD_REF && export DB_ENDPOINT=$DB_ENDPOINT_TEST
+test -z $CODEBUILD_WEBHOOK_HEAD_REF && export IMAGE_TAG=test
