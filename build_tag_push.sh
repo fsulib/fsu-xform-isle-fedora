05f5fe1ed9a11034362fdfb26876ca79808e7c4c
Malcolm Shackelford <mshackelford@fsu.edu>
2020-11-05 12:43:03 -0500

    Tagging images in the agreed-upon way now.

--- build_tag_push.sh
+++ build_tag_push.sh
@@ -2,16 +2,23 @@
 
 # Pushing to ECR. Would rather do this as individual commands.
 
-IMAGE_TAG=$(echo $CODEBUILD_WEBHOOK_HEAD_REF | cut -d / -f 3)
+image_target=$(echo $CODEBUILD_WEBHOOK_HEAD_REF | cut -d / -f 3)
 
-[[ $IMAGE_TAG ]] || IMAGE_TAG=test
+[[ $image_target ]] || image_target=test
 
-echo "IMAGE_TAG is $IMAGE_TAG"
+echo "image_target is $image_target"
 
+fsu_version="$(git tag | egrep '[^a-zA-Z]')"
 echo "Building image..."
 cd isle-fedora
+isle_release="$(git tag --points-at HEAD | cut -d - -f 2)"
+IMAGE_TAG="${image_target}-${isle_release}.${fsu_version}"
 docker build -t $IMAGE_REPO_NAME:$IMAGE_TAG .
 echo "Tagging image..."
 docker tag $IMAGE_REPO_NAME:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
 echo "Pushing image..."
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
+echo "Creating alternate tag..."
+alt_tag="${image_target}-latest"
+docker tag $IMAGE_REPO_NAME:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:${alt_tag}
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:${alt_tag}
